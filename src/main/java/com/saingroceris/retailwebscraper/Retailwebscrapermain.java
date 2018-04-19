83a9694442fd7ccb778bd9bf67f9e9da4a8657d7
veeraguru.s2 <veeraguru.s@sainsburys.co.uk>
2018-04-19 13:16:57 +0100

    removed unwanted dependencies

--- src/main/java/com/saingroceris/retailwebscraper/Retailwebscrapermain.java
+++ src/main/java/com/saingroceris/retailwebscraper/Retailwebscrapermain.java
@@ -10,7 +10,6 @@ import java.net.URL;
 public class Retailwebscrapermain {
     public static final void main(String[] args) {
         String inputurl = "https://www.sainsburys.co.uk/webapp/wcs/stores/servlet/CategoryDisplay?listView=true&orderBy=FAVOURITES_FIRST&parent_category_rn=12518&top_category=12518&langId=44&beginIndex=0&pageSize=20&catalogId=10137&searchTerm=&categoryId=185749&listId=&storeId=10151&promotionId=#langId=44&storeId=10151&catalogId=10137&categoryId=185749&parent_category_rn=12518&top_category=12518&pageSize=20&orderBy=FAVOURITES_FIRST&searchTerm=&beginIndex=0&hideFilters=true";
-        //String inputurl = "https://www.sainsburys.co.uk/shop/gb/groceries/ripe---ready/sainsburys-conference-pears--ripe---ready-x4-%28minimum%29";
 
         if (args.length == 1) {
             inputurl = args[0];
